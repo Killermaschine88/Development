b5f5de1581ee7c32fb6949a2f6e68b6fab78a766
Baltraz <86964184+Killermaschine88@users.noreply.github.com>
2022-03-17 22:19:41 +0000

    feat: git pull changes

--- messageCommands/pull.js
+++ messageCommands/pull.js
@@ -5,13 +5,14 @@ module.exports = {
   async execute(message, args, client) {
     //Imports
     const { exec } = require("child_process");
+    const Discord = require('discord.js')
 
     //Code
     exec("git pull origin main", (err, stdout, stderr) => {
       console.log({ err, stdout, stderr });
-      if (stdout.includes("Already up to date.")) return message.channel.send("Already upto date.");
+      if (stdout.includes("Already up to date.")) return message.channel.send({ embeds: [new Discord.MessageEmbed().setColor('GREEN').setDescription('Already upto date.')]})
       else {
-        return message.channel.send("Git pull", "```ansi\n" + stdout + "\n```\n\n```diff\n" + stderr + "\n```\n");
+        return message.channel.send({ embeds:[new Discord.MessageEmbed().setColor('GREEN').setTitle('Git pull').setDescription("```ansi\n" + stdout + "\n```\n\n```diff\n" + stderr + "\n```\n")]});
       }
     });
   },
