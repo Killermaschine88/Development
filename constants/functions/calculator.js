423525695057e99961b7d772e8fdabfabfed075c
Baltraz <86964184+Killermaschine88@users.noreply.github.com>
2022-04-05 08:23:40 +0000

    feature: removing last char + power to

--- constants/functions/calculator.js
+++ constants/functions/calculator.js
@@ -1,47 +1,55 @@
 const Discord = require("discord.js");
 
 const rows = [new Discord.MessageActionRow(), new Discord.MessageActionRow(), new Discord.MessageActionRow(), new Discord.MessageActionRow(), new Discord.MessageActionRow()];
-let j = 0
-let arr = ['ans', '(', ')', 'AC', '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+']
-for(const row of rows) {
-  for(let i = 0; i < 4; i++) {
-    row.components.push(new Discord.MessageButton().setStyle(getStyle(arr[j])).setLabel(arr[j]).setCustomId(arr[j]))
-    j++
+let j = 0;
+let arr = ["ans", "(", ")", "AC", "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+"];
+for (const row of rows) {
+  for (let i = 0; i < 4; i++) {
+    row.components.push(new Discord.MessageButton().setStyle(getStyle(arr[j])).setLabel(arr[j]).setCustomId(arr[j]));
+    j++;
   }
 }
+//manual added for the time being
+rows[0].components.push(new Discord.MessageButton().setStyle(getStyle('⬅')).setLabel('⬅').setCustomId('⬅'))
+rows[1].components.push(new Discord.MessageButton().setStyle(getStyle('^')).setLabel('^').setCustomId('^'))
 
 function getStyle(input) {
-  if(['ans', '(', ')', 'AC', '/', '*', '-', '+'].includes(input)) {
-    return "PRIMARY"
-  } else if(input === '=') {
-    return "SUCCESS"
+  if (["ans", "(", ")", "/", "*", "-", "+", "^"].includes(input)) {
+    return "PRIMARY";
+  } else if (input === "=") {
+    return "SUCCESS";
+  } else if (['⬅', "AC"].includes(input)) {
+    return "DANGER"
   } else {
-    return "SECONDARY"
+    return "SECONDARY";
   }
 }
 
 function calculate(id, str, ans) {
-  str = str !== '0' ? str : "";
-  ans = ans !== '0' ? ans : "";
+  str = str !== "0" ? str : "";
+  ans = ans !== "0" ? ans : "";
 
   if (id === "AC") {
-    return { str: '0', ans: '0' };
+    return { str: "0", ans: "0" };
   } else if (id === "=") {
     try {
-      ans = parse(str)
-      str = parse(str)
+      ans = parse(str);
+      str = parse(str);
     } catch (e) {
-      return { str: '0', ans: '0' }
+      return { str: "0", ans: "0" };
     }
     return { str: `${str}`, ans: `${ans}` };
   } else if (id === "ans") {
     return { str: `${(str += ans)}`, ans: `${ans}` };
+  } else if(id === '⬅') {
+    return { str: `${str.slice(0, -1)}`, ans: `${ans}`}
   } else {
     return { str: `${(str += id)}`, ans: `${ans}` };
   }
 }
 
 function parse(str) {
+  str = str.replace('^', '**')
   return Function(`'use strict'; return (${str})`)();
 }
 
--- slashCommands/calculator.js
+++ slashCommands/calculator.js
@@ -5,28 +5,28 @@ module.exports = {
   name: "calculator",
   devOnly: false,
   async execute(interaction) {
-    const message = await interaction.editReply({ content: '0', components: rows });
+    const message = await interaction.editReply({ content: "0", components: rows });
 
     const collector = message.createMessageComponentCollector({
       componentType: "BUTTON",
       time: 5 * 60000,
     });
 
-    let str = '0';
-    let ans = '0';
+    let str = "0";
+    let ans = "0";
 
     collector.on("collect", async (i) => {
       if (i.user.id !== interaction.user.id) return;
       await i.deferUpdate();
       const res = calculate(i.customId, str, ans);
-      str = res.str !== '' ? res.str : '0'
-      ans = res.ans !== '' ? res.ans : '0'
+      str = res.str !== "" ? res.str : "0";
+      ans = res.ans !== "" ? res.ans : "0";
 
       await interaction.editReply({ content: str });
     });
 
     collector.on("end", async () => {
-      await interaction.editReply({ content: 'Calculator expired', components: [] });
+      await interaction.editReply({ content: "Calculator expired", components: [] });
     });
   },
 };
