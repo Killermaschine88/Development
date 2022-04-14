bed6e72a99bad0aec8c3211b08e7c5f1621080cf
Baltraz <86964184+Killermaschine88@users.noreply.github.com>
2022-04-14 22:06:03 +0000

    feat: url scanner

--- constants/functions/automation.js
+++ constants/functions/automation.js
@@ -1,4 +1,6 @@
+const Discord = require("discord.js")
 const sourcebin = require("sourcebin");
+const got = require("got")
 
 async function checkAutomation(msg, client) {
   if (msg.channel.id === "963810201260134400") {
@@ -22,8 +24,19 @@ async function checkAutomation(msg, client) {
   }
 
   if(msg.channel.id === "964268555833069668") { //URL Checker
-    const res = await got(msg.content)
-    console.log(res)
+    await msg.delete()
+    try {
+      const res = await got(msg.content)
+      const redirects = res.redirectUrls.join(" -> ")
+
+      const embed = new Discord.MessageEmbed().setTimestamp().setDescription(`Info for ${msg.content}\nStatus Code: ${res.statusCode || "Unknown"}`).addField("Redirects", `${msg.content} -> ${redirects || "None"}`, true)
+
+      await msg.channel.send({embeds: [embed]})
+      return true
+    } catch(e) {
+      await msg.channel.send(`URL: \`${msg.content}\`\n\n**Error**\n\`\`\`js\n${JSON.stringify(e.stack, null, 4)}\n\`\`\``)
+      return true
+    }
   }
 }
 
