3376acf45fa1c682c4e2fb46750b8c2bbb7d45e9
Baltraz <ybaltraz@gmail.com>
2022-04-26 18:02:48 +0000

    last

---
+++ ctold/index.js
@@ -0,0 +1,76 @@
+import request from "requestV2/index"
+let started = false;
+let render = false;
+let i = 0;
+let sent = []
+const prefix = "[§eAH Bot§f]"
+const suffix = "§l§f[§6CLICK ME§f]"
+const rarities = {
+  COMMON: "§7",
+  UNCOMMON: "§a",
+  RARE: "§9",
+  EPIC: "§5",
+  LEGENDARY: "§6",
+  MYTHIC: "§d"
+}
+
+register("step", () => {
+  if (!started) return;
+  //Whatever you wanna call every 30 seconds and started has to be true
+  try {
+    request("https://development.baltrazz.repl.co/shards").then(res => {
+        if (res.length > 0) {
+            res = JSON.parse(res);
+            res.forEach(item => {
+                if(sent.includes(item.command)) return;
+                sendMessage(item);
+                sent.push(item.command)
+            })
+          } else {
+            return ChatLib.chat(`${prefix} No new Items found.`);
+          }
+    })
+  } catch (e) {
+    started = false;
+    return ChatLib.chat(`${prefix} Error occured, stopped Module.`);
+  }
+}).setDelay(15);
+
+register("command", () => {
+  if (started) {
+    return ChatLib.chat(`${prefix} Module already started.`);
+  }
+  started = true;
+  ChatLib.chat(`${prefix} Started AH Bot.`);
+}).setName("startah");
+
+register("command", () => {
+  if (!started) {
+    return ChatLib.chat(`${prefix} Module not started yet.`);
+  }
+  started = false;
+  ChatLib.chat(`${prefix} Stopped AH Bot.`);
+}).setName("stopah");
+
+let imagE;
+const GUIClass = Java.type("net.minecraft.client.gui.GuiChat").class.toString();
+render = false;
+
+register("postguirender", (mouseX, mouseY, guiname) => {
+  ChatLib.chat("before " + guiname.class.toString())
+  if (!guiname.class.toString() == GUIClass) return;
+  if (!render) return;
+  ChatLib.chat("after " + guiname.class.toString())
+  imagE.draw(1, 1);
+  render = false;
+});
+
+register("chatcomponenthovered", () => {
+  render = true;
+});
+
+function sendMessage(item) {
+  if (!started) return;
+  imagE = new Image(`${item.name}${i++}`, `${item.image}`);
+  new Message(new TextComponent(`${prefix} ${rarities[item.rarity]}${item.name} §f- §a${item.price} ${suffix}`).setClick("run_command", `${item.command}`).setHover("show_text", "§eImage")).chat();
+}
