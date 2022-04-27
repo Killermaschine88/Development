a6947fd1bf62609a494987c6ad349dd5db7e9345
Baltraz <ybaltraz@gmail.com>
2022-04-27 15:15:35 +0000

    ast

--- ctold/index.js
+++ ctold/index.js
@@ -2,7 +2,9 @@ import request from "requestV2/index"
 let started = false;
 let render = false;
 let i = 0;
-let sent = []
+let j = 0;
+let sent = [];
+let running = false;
 const prefix = "[§eAH Bot§f]"
 const suffix = "§l§f[§6CLICK ME§f]"
 const rarities = {
@@ -14,8 +16,24 @@ const rarities = {
   MYTHIC: "§d"
 }
 
+const blacklist = ["Your Implosion", "There are blocks"]
+
+//Implosion Hider
+register("chat", (message, event) => {
+  if(blacklist.some(word => message.includes(word))) {
+    cancel(event)
+  }
+}).setCriteria("${message}")
+
 register("step", () => {
   if (!started) return;
+  if(running) return;
+  running = true
+  ChatLib.chat(`${prefix} Started searching for Items.`);
+  if(j % 5 === 0) {
+    sent = []
+  }
+  j++
   //Whatever you wanna call every 30 seconds and started has to be true
   try {
     request("https://development.baltrazz.repl.co/shards").then(res => {
@@ -31,11 +49,13 @@ register("step", () => {
             return ChatLib.chat(`${prefix} No new Items found.`);
           }
     })
+    ChatLib.chat(`${prefix} Finished searching Items and sent them.`);
   } catch (e) {
     started = false;
     return ChatLib.chat(`${prefix} Error occured, stopped Module.`);
   }
-}).setDelay(30);
+  running = false
+}).setDelay(20);
 
 register("command", () => {
   if (started) {
@@ -53,7 +73,7 @@ register("command", () => {
   ChatLib.chat(`${prefix} Stopped AH Bot.`);
 }).setName("stopah");
 
-let imagE;
+/*let imagE;
 const GUIClass = Java.type("net.minecraft.client.gui.GuiChat").class.toString();
 render = false;
 
@@ -68,10 +88,10 @@ register("postguirender", (mouseX, mouseY, guiname) => {
 
 register("chatcomponenthovered", () => {
   render = true;
-});
+});*/
 
 function sendMessage(item) {
   if (!started) return;
-  imagE = new Image(`${item.name}${i++}`, `${item.image}`);
-  new Message(new TextComponent(`${prefix} ${rarities[item.rarity]}${item.name} §f- §a${item.price} ${suffix}`).setClick("run_command", `${item.command}`).setHover("show_text", "§eImage")).chat();
+  //imagE = new Image(`${item.name}${i++}`, `${item.image}`);
+  new Message(new TextComponent(`${prefix} ${rarities[item.rarity]}${item.name} §f- §a${item.price} ${suffix}`).setClick("run_command", `${item.command}`).setHover("show_text", `${item.name}`)).chat();
 }
