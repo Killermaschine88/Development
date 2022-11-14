569d793f3526fc572c5f4ce0b08955d5c1f9437e
baltraz <you@example.com>
2022-11-14 19:13:22 +0000

    fix

--- slashCommands/pull.js
+++ slashCommands/pull.js
@@ -8,40 +8,20 @@ module.exports = {
     // Ignore if not dev access role
     if (!interaction.member.roles.cache.has("1010570031664484402")) return interaction.editReply("Go away nerd!");
 
-    // CD into Minecraft Directory
-    exec("cd /home/Minecraft", (err, stdout, stderr) => {
+    // CD into Minecraft Directory and Git Pull
+    exec("cd /home/Minecraft && git pull", (err, stdout, stderr) => {
       console.log({ err, stdout, stderr });
-      // Git Pull
-      exec("git pull", (err, stdout, stderr) => {
-        console.log({ err, stdout, stderr });
-        if (stdout.includes("Already up to date.")) return interaction.editReply({ embeds: [new MessageEmbed().setColor("GREEN").setDescription("Already upto date.")] });
-        else {
-          return interaction.editReply({
-            embeds: [
-              new MessageEmbed()
-                .setColor("GREEN")
-                .setTitle("Git pull")
-                .setDescription("```ansi\n" + stdout + "\n```\n\n```diff\n" + stderr + "\n```\n"),
-            ],
-          });
-        }
-      });
+      if (stdout.includes("Already up to date.")) return interaction.editReply({ embeds: [new MessageEmbed().setColor("GREEN").setDescription("Already upto date.")] });
+      else {
+        return interaction.editReply({
+          embeds: [
+            new MessageEmbed()
+              .setColor("GREEN")
+              .setTitle("Git pull")
+              .setDescription("```ansi\n" + stdout + "\n```\n\n```diff\n" + stderr + "\n```\n"),
+          ],
+        });
+      }
     });
-
-    // // Git Pull
-    // exec("git pull", (err, stdout, stderr) => {
-    //   console.log({ err, stdout, stderr });
-    //   if (stdout.includes("Already up to date.")) return interaction.editReply({ embeds: [new MessageEmbed().setColor("GREEN").setDescription("Already upto date.")] });
-    //   else {
-    //     return interaction.editReply({
-    //       embeds: [
-    //         new MessageEmbed()
-    //           .setColor("GREEN")
-    //           .setTitle("Git pull")
-    //           .setDescription("```ansi\n" + stdout + "\n```\n\n```diff\n" + stderr + "\n```\n"),
-    //       ],
-    //     });
-    //   }
-    // });
   },
 };
